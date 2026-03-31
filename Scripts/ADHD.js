591228004b8831ea7623c359889d445677aadba1
tony0nyx <103608@glr.nl>
2026-03-31 15:38:23 +0200

    0-0

---
+++ Scripts/ADHD.js
@@ -0,0 +1,19 @@
+const divinfo = document.querySelector('.divinfo')
+const infoCom = document.querySelector('.comb_info')
+const hypeInfo = document.querySelector('.hype_info')
+
+divinfo.addEventListener('click', () => {
+    divinfo.classList.toggle('open')
+})
+
+
+infoCom.addEventListener('click', () => {
+    infoCom.classList.toggle('open')
+})
+
+
+hypeInfo.addEventListener('click', () => {
+    hypeInfo.classList.toggle('open')
+})
+
+
